7f3dbd421dc1833ca8ea5d739ee462567bbdeebd
ray cod <rayzcodz@gmail.com>
2019-08-25 12:39:47 -0500

    Iterate over two lists using zip.

---
+++ data_structures/zip_lists.py
@@ -0,0 +1,12 @@
+# Iterate over two lists.
+list_a = ['a', 'b', 'c', 'd']
+list_b = [1, 2, 3, 4]
+list_c = [100, 200, 300, 400]
+
+for x, y in zip(list_a, list_b):
+    print(x, y)
+
+
+# optional additional practice
+for x, y in zip(zip(list_a, list_b), list_c):
+    print(x, y)
