aea16977f6fcd1d90a52145b9b7cfb004ace9e3a
ray cod <rayzcodz@gmail.com>
2019-08-24 20:06:28 -0500

    WAP to print index and elems of list.

---
+++ data_structures/enumerate_index.py
@@ -0,0 +1,4 @@
+# WAP to print list elements a long with their indeces
+my_list = ['apple', 'amazon', 'micro soft', 'google', 'babazon']
+for index, value in enumerate(my_list):
+    print(index, value)
